e0e6adbefd02a25314f4abef527b74df26266aac
Binbin <pauljin888@gmail.com>
2023-01-26 22:38:48 +0000

    Return best guess station

--- src/main.rs
+++ src/main.rs
@@ -14,26 +14,31 @@ fn get_possible_states(
     map
 }
 
-fn num_guesses_required(possible_stations: &[Station]) -> usize {
+fn num_guesses_required(possible_stations: &[Station]) -> (Station, usize) {
     if possible_stations.len() == 1 {
-        return 0;
+        return (possible_stations[0], 0);
     }
     possible_stations
         .iter()
         .map(|station| {
             get_possible_states(station, possible_stations)
                 .iter()
-                .map(|s| num_guesses_required(s.1))
+                .map(|x| {
+                    if x.1.len() == possible_stations.len() {
+                        return usize::MAX - 1;
+                    }
+                    num_guesses_required(x.1).1
+                })
                 .max()
+                .map(|x| (*station, x + 1))
                 .unwrap()
-                + 1
         })
-        .min()
+        .min_by_key(|x| x.1)
         .unwrap()
 }
 
 fn main() {
     let stations: Vec<Station> = (0..269).map(Station::from).collect();
-    let total_guesses_required = num_guesses_required(&stations);
-    println!("{}", total_guesses_required);
+    let (best_station, total_guesses_required) = num_guesses_required(&stations);
+    println!("{} {}", best_station.get_name(), total_guesses_required);
 }
